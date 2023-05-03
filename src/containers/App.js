896b4599635eff5e2df3da4e613ebcc60b82fc58
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-05-03 15:45:28 +0530

    Update files from laptop

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 
@@ -6,47 +6,58 @@ import Scroll from '../components/Scroll'
 
 import "./App.css";
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// } //not needed when we use hooks
 
-	componentDidMount() {
-		//console.log("Check");
+	const [robots, setRobots] = useState([]); //robots is the variable and the other term is a function
+	const [searchfield, setSearchfield] = useState('');
+	const [count, setCount] = useState(0);
+
+	// componentDidMount() {
+	// 	//console.log("Check");
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 	.then(response => response.json())
+	// 	.then(users => this.setState({ robots: users}));
+	// }
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({ robots: users}));
-	}
+	 	.then(response => response.json())
+	 	.then(users => setRobots(users));
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value})
+		console.log(count);
+	}, [ count ]) //without the array it keeps updating in an infinite loop
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value)
 		// console.log(event.target.value);
 		// console.log(filteredRobots);
 	}
 
-	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
 
-		if(this.state.robots.length === 0){
-			return <h1>Loading..</h1>
-		}
-
-		return(
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll>
-					<CardList robots={filteredRobots} />
-				</Scroll>
-			</div>
-		)
+	if(robots.length === 0){
+		return <h1>Loading..</h1>
 	}
+
+	return(
+		<div className="tc">
+			<h1 className="f1">RoboFriends</h1>
+			<button onClick={() => setCount(count+1)}>Click Me!</button>
+			<SearchBox searchChange={onSearchChange}/>
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+		</div>
+	)
 }
+
 
 export default App;
